3e7109f95a41905bab31cfc2ad5ad3590b31a5af
Bùi Trúc Anh <trucngonghinh2002@gmail.com>
2023-11-05 21:03:49 +0700

    finish

--- ChatBot Generate Data/Models/Config.cs
+++ ChatBot Generate Data/Models/Config.cs
@@ -9,6 +9,7 @@ namespace ChatBot_Generate_Data.Models
     public class Config
     {
         public int NumberTasks { get; set; }
+        public int NumberErrorVariations { get; set; }
         public string Url { get; set; }
     }
 }
