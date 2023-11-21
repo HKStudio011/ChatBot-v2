c887c7926867a7fbccf21bf6db52d8ca1084209a
Bùi Trúc Anh <trucngonghinh2002@gmail.com>
2023-11-21 11:46:07 +0700

    nhung model

---
+++ Chatbot BlazorApp/Chatbot BlazorApp/Models/Contents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot_Generate_Data.Models
+{
+    public class Contents
+    {
+        [Key]
+        public int IdContent { get; set; }
+        public string Content { get; set; }
+        public List<SplitContents> SplitContents { get; set; }
+    }
+}
