356f4d761767549a6eefdd5a326fcd8beaefd0a3
Bùi Trúc Anh <trucngonghinh2002@gmail.com>
2023-11-22 11:16:05 +0700

    add database

--- Chatbot BlazorApp/Chatbot BlazorApp/Program.cs
+++ Chatbot BlazorApp/Chatbot BlazorApp/Program.cs
@@ -1,7 +1,28 @@
 using Chatbot_BlazorApp.Client.Pages;
 using Chatbot_BlazorApp.Components;
+using Chatbot_BlazorApp.Models;
+using Chatbot_BlazorApp.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services chatbot
+builder.Services.AddSingleton<ChatbotModel>( provider =>
+{
+    string PathModel = builder.Configuration.GetSection("Model.PathModel").ToString();
+    int NumberThread = int.Parse(builder.Configuration.GetSection("Model.NumberThread").ToString());
+    string Version   = builder.Configuration.GetSection("Model.Version").ToString();
+    var model = new ChatbotModel(PathModel,NumberThread,Version);
+    model.Init().Wait();
+    return model;
+});
+
+// Add DBContent
+builder.Services.AddDbContext<ChatbotContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ChatbotContext"));
+});
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
